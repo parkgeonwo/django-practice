8db69367de07c14ef0d5818c988ac8db0c0ab1fb
Geonwoo Park <parkkeonwo@naver.com>
2022-09-29 22:41:24 +0900

    220929 22:41 course 9

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 def hello_world(request):  # 요청을받고
     # return HttpResponse("hello world")    # 직접 html을 만들어서 보여줌
-    return render(request, 'base.html')    # base.html에 집어넣어서 보여줌 
+    return render(request, 'accountapp/hello_world.html')    # accountapp/hello_world.html에 집어넣어서 보여줌 
